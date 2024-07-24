67b8ac284351c8809ea77447688668cdb9dc33dd
Arsal uddin <arsaluddin134@gmail.com>
2024-07-24 15:54:35 +0530

    env issue resolved

--- server.js
+++ server.js
@@ -5,19 +5,19 @@ const dotenv = require('dotenv');
 
 dotenv.config();
 
+
 const app = express();
 
 // Middleware
 app.use(bodyParser.json());
 
-// DB Config
-mongoose.connect(process.env.MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true })
+mongoose.connect(process.env.MONGO_URI)
   .then(() => console.log('MongoDB connected'))
   .catch(err => console.log(err));
 
 // Routes
-const authRoutes = require('./routes/auth');
-const blogRoutes = require('./routes/blog');
+const authRoutes = require('./src/routes/auth');
+const blogRoutes = require('./src/routes/blog');
 
 app.use('/api/auth', authRoutes);
 app.use('/api/blogs', blogRoutes);
